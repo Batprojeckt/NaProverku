5330fac15eaacabf03d718f4222831768e1e1a81
Batprojeckt <passhustrov@gmail.com>
2023-12-16 20:26:09 +0300

    Create main.js

---
+++ Domashka/JS_Lesson-2/main.js
@@ -0,0 +1,41 @@
+const sum = 2 + 2
+const minus = 12 - 6
+const a = 4
+
+const x = 5
+const y = 10
+// const result = x * y
+// const result = y / x
+// const result = x + y
+// const result = x - y
+
+console.log(x * 3)
+console.log(3 ** 3)
+console.log(sum)
+console.log(minus)
+
+console.log('Hello ' + 'World')
+
+
+const myInt = 5
+const myFloat = 6.667
+const myRice = 12
+
+console.log(typeof myInt, typeof myFloat)
+const procent = 11 % 3
+console.log(procent)
+console.log(11 % 4)
+const myName = 23 % 5
+console.log(procent * myName)
+const num1 = 10
+const num2 = 50
+console.log(num2 + num1 / 8 + 2)
+console.log((num2 + num1) / (8 + 2))
+let num3 = 4
+console.log(--num3)
+let c = 5
+let v = 1
+console.log(v-=c)
+console.log(v+=c)
+console.log(v*=c)
+console.log(v/=c)
