d0604d1c2f2e99082e7449e7f549e41954e072f6
Batprojeckt <passhustrov@gmail.com>
2023-12-16 21:07:40 +0300

    Create main.js

---
+++ Domashka/JS_Lesson-3/main.js
@@ -0,0 +1,21 @@
+// 1) string    строки ('word' + 'word') / word
+// 2) number    12345667889045325435
+// 3) boolean   true/false
+// 4) undefined
+// 5) null
+// 6) bigint*
+// 7) symbol*
+
+const instrument = 'Инструмент'
+const box = 'Ящик'
+
+// console.log(`${instrument} лежит в ${box}e` )
+
+// instrument = 10
+// console.log(instrument)
+
+const user = 'User'
+const age = 25
+const isAdmin = true
+
+console.log(`${user} admin:${isAdmin}`)
