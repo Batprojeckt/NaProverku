7b88a5cd8d0133495539021bacdd22d34f98cfdf
Batprojeckt <passhustrov@gmail.com>
2023-12-17 12:28:06 +0300

    Create main.js

---
+++ Domashka/JS_Lesson-4/main.js
@@ -0,0 +1,10 @@
+const userAge = 28
+const userName = 'Павел'
+
+// if (userrAge >= 18) {
+//     console.log(`Вы совершеннолетний, ${userName}!`)
+// } else {
+//     console.log(`${userName}, Вам еще нет 18`)
+// }
+
+userAge >= 18 ? console.log(`Вы совершеннолетний, ${userName}!`) : console.log(`${userName}, Вам еще нет 18`)
