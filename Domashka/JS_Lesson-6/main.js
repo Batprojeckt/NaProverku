368e43b296375fd874fb1d5bd26e6abdb23463e6
Batprojeckt <passhustrov@gmail.com>
2024-01-06 16:15:42 +0300

    Create main.js

---
+++ Domashka/JS_Lesson-6/main.js
@@ -0,0 +1,17 @@
+// const numbers = [1, 2, 3, 4, 5]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i] + 1)
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for (let i = 1; i < numbers.length; i = i + 2) {
+    console.log(numbers[i])
+}
+
+const rainbow = ['Красный', 'Оранжевый', 'Желтый', 'Зеленый', 'Голубой', 'Синий', 'Фиолетовый']
+
+for (let i = rainbow.length-1; i >= 0; i--) {
+    console.log(rainbow[i]);
+}
