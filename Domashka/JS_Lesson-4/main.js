3266272559daab734277d986def3b615c06d83ab
Batprojeckt <passhustrov@gmail.com>
2024-01-06 13:44:16 +0300

    Update main.js

--- Domashka/JS_Lesson-4/main.js
+++ Domashka/JS_Lesson-4/main.js
@@ -7,4 +7,4 @@ const userName = 'Павел'
 //     console.log(`${userName}, Вам еще нет 18`)
 // }
 
-userAge >= 18 ? console.log(`Вы совершеннолетний, ${userName}!`) : console.log(`${userName}, Вам еще нет 18`)
+userAge >= 18 ? console.log(`Вы совершеннолетний, ${userName}!`) : console.log(`${userName}, Вам еще нет 18!`)
